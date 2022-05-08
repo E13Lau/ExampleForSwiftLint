c50195d986d735b9fb680c184c1a8a806ca33e5f
e13lau's imac <lauy@pm.me>
2022-05-08 20:54:33 +0800

    Initial Commit

---
+++ Shared/ExampleForSwiftlintApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleForSwiftlintApp.swift
+//  Shared
+//
+//  Created by lau on 2022/5/8.
+//
+
+import SwiftUI
+
+@main
+struct ExampleForSwiftlintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
